f282359db839c9f8424a736f2d6c7c2abc0fc99d
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-04-24 10:23:06 -0700

    Update useFetch.jsx

--- src/Components/useFetch.jsx
+++ src/Components/useFetch.jsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from 'react'
 
-const useFetch = (url) => {
+    const useFetch = (url) => {
     const [data,setData]=useState(null)
     const [isPending,setIsPending]=useState(true)
     const [error,setError]=useState(null)
-    
 
-    useEffect(() => {
+useEffect(() => {
         //for unmonunted state change
         const abortCont=new AbortController();
         //delay for display loading for 1 second
-        // setTimeout(() => {
+        setTimeout(() => {
             fetch(url,{signal:abortCont.signal})
                 .then(res=>{
                     if(!res.ok){
@@ -29,8 +28,8 @@ const useFetch = (url) => {
                     }
                     setIsPending(false)
                     setError(e.message)
-                })
-        // }, 1000);
+            })
+        }, 1000);
         return () => abortCont.abort()
     }, [url]);
     return {data,isPending,error}
